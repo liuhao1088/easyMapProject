d90c8b24663ca3c825881363c696c709c549ad6f
liuhao1088 <1350569467@qq.com>
2020-12-23 16:54:49 +0800

    更新

--- pages/sEightZeroZeroA/sEightZeroZeroA.js
+++ pages/sEightZeroZeroA/sEightZeroZeroA.js
@@ -5,6 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
+    item:{
+      url:'https://img10.360buyimg.com/ddimg/jfs/t1/140397/13/14257/270611/5face165E6c93f4cb/da09de25dc1be0dc.jpg',
+      videoUrl:'https://fnck-20200914-1303154931.cos.ap-guangzhou.myqcloud.com/%E8%A7%86%E9%A2%91/%E6%98%93%E5%9B%BE/%E6%98%93%E5%9B%BES800A%E5%A4%96%E8%A7%82.mp4?q-sign-algorithm=sha1&q-ak=AKIDHL7fPrFwXrtdX9SYLq75j5BxTeFbR3KK&q-sign-time=1604991895;10244905495&q-key-time=1604991895;10244905495&q-header-list=&q-url-param-list=&q-signature=9eadd29cfd6e198a27b6266b3a1257f00e75a799',
+      detailList:[
+        'https://img10.360buyimg.com/ddimg/jfs/t1/121104/12/18511/275343/5face101Eb57ed11d/0a7f2231227f47e6.jpg',
+        'https://img10.360buyimg.com/ddimg/jfs/t1/153390/5/5642/291180/5face11aEfc8ba405/2539d6de1ae27a29.jpg',
+        'https://img13.360buyimg.com/ddimg/jfs/t1/144155/31/14292/272349/5face12eE6126f32c/18b07d0c892dac0f.jpg',
+        'https://img10.360buyimg.com/ddimg/jfs/t1/143781/29/14151/354028/5face144E0ab35cb0/8539238c48a00295.jpg',
+        'https://img14.360buyimg.com/ddimg/jfs/t1/132851/11/15826/445648/5face155E274fae8f/875a082f9759be69.jpg',
+        'https://img11.360buyimg.com/ddimg/jfs/t1/146462/31/14243/267622/5face0e1E3b924a3d/7ffce5ec6fb1b51f.jpg'
+      ]
+    }
 
   },
 
--- pages/smartStreaming/smartStreaming.js
+++ pages/smartStreaming/smartStreaming.js
@@ -5,17 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    detailsList: [
+      'https://img11.360buyimg.com/ddimg/jfs/t1/122465/21/18161/177718/5faa026bEcde9b6fa/710f1e1b6e226a6e.png',
+      'https://img14.360buyimg.com/ddimg/jfs/t1/120744/18/17968/171327/5faa027dE90152b18/9e13f3d05bbebe04.png',
+    ]
   },
-  pSixZeroZerodj: function () {
-    wx.navigateTo({
-      url: "../pSixZeroZero/pSixZeroZero"
-    })
-  },
-  sEightZeroZeroAdj: function () {
-    wx.navigateTo({
-      url: "../sEightZeroZeroA/sEightZeroZeroA"
-    })
+  toDetails(e) {
+    let index = e.currentTarget.dataset.index;
+    if (index === 0) {
+      wx.navigateTo({
+        url: "../pSixZeroZero/pSixZeroZero"
+      })
+    } else {
+      wx.navigateTo({
+        url: "../sEightZeroZeroA/sEightZeroZeroA"
+      })
+    }
   },
   /**
    * 生命周期函数--监听页面加载
